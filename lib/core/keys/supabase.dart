fe2ebb06278901dfc674cdf294a93440375fb82d
sujan bimali <137367456+sujanbimali9@users.noreply.github.com>
2024-04-14 16:42:34 +0545

    Update supabase.dart

--- lib/core/keys/supabase.dart
+++ lib/core/keys/supabase.dart
@@ -1,5 +1,4 @@
 class SupabaseKey {
-  static String supabaseUrl = "https://zgiuwljbrtjswmfzkcpo.supabase.co";
+  static String supabaseUrl = "";
   static String supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnaXV3bGpicnRqc3dtZnprY3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODkxMTYsImV4cCI6MjAyNjc2NTExNn0.R0hE0rY-05UbcIqB0klJWSPwgYpZhvQ7KYYLArTNSXQ";
-}
+'';}
